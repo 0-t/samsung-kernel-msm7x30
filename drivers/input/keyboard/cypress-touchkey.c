729539cdb25c974bc66731540ef00024f965d0fd
jfbs <jeffmbowers@gmail.com>
2012-12-15 09:38:49 -0500

    Revert "Wakelock management on-demand activation!"
    This reverts commit f9b3e2c7469a3cd87c33862ceebbd09599b2651e.
    
    Conflicts:
    	drivers/input/keyboard/cypress-touchkey.c

--- drivers/input/keyboard/cypress-touchkey.c
+++ drivers/input/keyboard/cypress-touchkey.c
@@ -56,6 +56,7 @@ static DECLARE_MUTEX(i2c_sem);
 
 static int bl_timeout = 1600; // This gets overridden by userspace AriesParts
 static int bl_wakelock = 0;
+
 static struct timer_list bl_timer;
 static void bl_off(struct work_struct *bl_off_work);
 static DECLARE_WORK(bl_off_work, bl_off);
@@ -680,7 +681,7 @@ static void notify_led_on(void) {
 	i2c_touchkey_write_byte(devdata_global, devdata_global->backlight_on);
 	bl_on = 1;
 
-   if( wake_lock_active(&bln_wake_lock) ){
+   if(wake_lock_active(&bln_wake_lock) ){
         printk(KERN_DEBUG "[TouchKey] touchkey clear wake_lock\n");
         wake_unlock(&bln_wake_lock);
     }
@@ -770,6 +771,9 @@ static ssize_t bl_wakelock_write(struct device *dev, struct device_attribute *at
 static DEVICE_ATTR(led, S_IRUGO | S_IWUGO , led_status_read, led_status_write);
 static DEVICE_ATTR(bl_timeout, S_IRUGO | S_IWUGO, bl_timeout_read, bl_timeout_write);
 static DEVICE_ATTR(wakelock, S_IRUGO | S_IWUGO, bl_wakelock_read, bl_wakelock_write);
+
+static DEVICE_ATTR(led, S_IRUGO | S_IWUGO , led_status_read, led_status_write);
+static DEVICE_ATTR(bl_timeout, S_IRUGO | S_IWUGO, bl_timeout_read, bl_timeout_write);
 
 static struct attribute *bl_led_attributes[] = {
 		&dev_attr_led.attr,
