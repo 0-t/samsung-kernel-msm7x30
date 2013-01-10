4999e5d4f7bf05d793ced9f90f937d158ac89e89
jfbs <jeffmbowers@gmail.com>
2013-01-10 01:03:04 -0500

    Add smooth gov.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -401,6 +401,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 /* #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
 extern struct cpufreq_governor cpufreq_gov_ondemandx;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_ondemandx) */
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMOOTHASS)
+extern struct cpufreq_governor cpufreq_gov_smoothass;
+#define CPUFREQ_DEFAULT_GOVERNOR   (&cpufreq_gov_smoothass)
 #endif
 
 /*********************************************************************
