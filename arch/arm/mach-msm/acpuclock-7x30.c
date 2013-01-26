74dfa22156a90c6da186a54e62bd55c8cc66d505
jfbs <jeffmbowers@gmail.com>
2013-01-25 19:44:11 -0500

    Remove pll speed

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -505,21 +505,12 @@ void __init pll2_fixup(void)
 {
 	struct clkctl_acpu_speed *speed = acpu_freq_tbl;
 
-	u8 pll2_l = readl_relaxed(PLL2_L_VAL_ADDR) & 0xFF;
+	// u8 pll2_l = readl_relaxed(PLL2_L_VAL_ADDR) & 0xFF;
 
 	for ( ; speed->acpu_clk_khz; speed++) {
 		if (speed->src != PLL_2)
 			backup_s = speed;
-
-		if (speed->pll_rate && speed->pll_rate->l == pll2_l) {
-			speed++;
-			speed->acpu_clk_khz = 0;
-			return;
-		}
 	}
-
-	pr_err("Unknown PLL2 lval %d\n", pll2_l);
-	BUG();
 }
 
 #define RPM_BYPASS_MASK	(1 << 3)
@@ -545,6 +536,8 @@ static struct acpuclk_data acpuclk_7x30_data = {
 
 static int __init acpuclk_7x30_init(struct acpuclk_soc_data *soc_data)
 {
+	struct clkctl_acpu_speed *s;
+
 	pr_info("%s()\n", __func__);
 
 	mutex_init(&drv_state.lock);
