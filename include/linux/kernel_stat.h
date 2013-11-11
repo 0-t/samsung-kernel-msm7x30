f0c729623dc70dd21e472a58036389ce74b55134
jfbs <jeffmbowers@gmail.com>
2013-11-11 16:36:56 -0500

    Revert "kernel_stat: define cpustat"
    This reverts commit e6b6ed7784e5327e662c985029a85b2faaee2b31.

--- include/linux/kernel_stat.h
+++ include/linux/kernel_stat.h
@@ -35,9 +35,8 @@ struct kernel_cpustat {
 };
 
 struct kernel_stat {
-	struct cpu_usage_stat cpustat;
 #ifndef CONFIG_GENERIC_HARDIRQS
-	unsigned int irqs[NR_IRQS];
+       unsigned int irqs[NR_IRQS];
 #endif
 	unsigned long irqs_sum;
 	unsigned int softirqs[NR_SOFTIRQS];
